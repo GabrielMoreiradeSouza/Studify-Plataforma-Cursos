74e652324e23eea7dca4fd4d83722d5f0b9d65c3
Gabriel Moreira de Souza <gmoreiradesouza19@gmail.com>
2026-05-30 10:21:40 -0300

    update frontend

--- frontend/src/components/Nav/index.tsx
+++ frontend/src/components/Nav/index.tsx
@@ -4,16 +4,23 @@ import { Link } from "react-router-dom";
 export const Nav = () => {
     return (
         <>
-            <div className="d-flex justify-content-center align-items-center bg-dark p-3 mb-3 text-white">
-                <ul className="nav">
+            <div className="d-flex justify-content-between align-items-center bg-dark px-3 py-2 mb-3 text-white">
+                <Link to="/home">
+                    <img
+                        src="/LogoName.png"
+                        alt="Logo Studify"
+
+                    />
+                </Link>
+                <ul className="nav ms-auto">
                     <li className="nav-item">
-                        <Link className="nav-link text-white" to="/">Home</Link>
+                        <Link className="nav-link text-white" to="/">Cursos</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link text-white" to="/usuario">Usuario</Link>
+                        <Link className="nav-link text-white" to="/planos">Inscreva-se</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link text-white" to="/sobre">Sobre</Link>
+                        <Link className="nav-link text-white" to="/sobre">Contato</Link>
                     </li>
                 </ul>
             </div>
--- frontend/src/pages/HomePages/index.tsx
+++ frontend/src/pages/HomePages/index.tsx
@@ -2,8 +2,19 @@
 
 export const HomePages = () => {
   return (
-    <div className="d-flex">
-        <h3>Home Pages</h3>
+    <div className="d-flex flex-column align-items-center text-center">
+        <h3>Cursos de Web Design, UX/UI <br /> Design, HTML, CSS,<br /> JavaScript, TS, Node e React</h3>
+        <div
+        style={{
+            width: "40px",
+            height: "2px",
+            backgroundColor: "black",
+            margin: "20px 0"
+        }}
+    />
+        <p>Aprenda do zero Web Design e comece a sua carreira <br /> de UX / UI Designer e Desenvolvedor(a) Front End.<br /> Mais de 1600 aulas divididas em 25 cursos.</p>
+
+        <button className="btn btn-primary">Inscreva-se Agora ►</button>
     </div>
     );
 };
