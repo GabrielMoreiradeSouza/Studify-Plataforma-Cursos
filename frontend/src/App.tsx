0b03a548aa7afcf6cbdedf3cdaff4cdcfe2038a3
Gabriel Moreira de Souza <gmoreiradesouza19@gmail.com>
2026-05-30 08:15:24 -0300

    feat: add frontend path

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Nav } from "./components/Nav"
+import { AppRouter } from "./routers/app.routers"
+
+
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+      <AppRouter/>
+    </>
+  )
+}
+
+export default App
